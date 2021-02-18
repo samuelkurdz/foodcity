0bbcd495202a2628caf6f4168fc70290695ce21b
adebayo <kurdzsamuel@gmail.com>
2021-02-18 05:26:11 +0100

    added hamburger to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
+import { useMediaQuery } from '../../utils/useMediaQuery';
 import CustomButton from '../custom-button/custom-button';
 import './header.scss';
 
 // import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  let isPageWide = useMediaQuery('(min-width: 800px)');
+
   return (
     <div className="header">
       <h3>FoodCity</h3>
-      <div className="header-links">
-        <span className="header-link">Menu</span>
-        <span className="header-link">Locations</span>
-        <span className="header-link">Our Story</span>
-        <span className="header-link">Cart</span>
-        <CustomButton buttonText='Order Now' buttonType='accent'/>
-      </div>
+      { isPageWide ?
+        (
+          <div className="header-links">
+            <span className="header-link">Menu</span>
+            <span className="header-link">Locations</span>
+            <span className="header-link">Our Story</span>
+            <span className="header-link">Cart</span>
+            <CustomButton buttonText='Order Now' buttonType='accent'/>
+          </div>
+        ) :
+        <div id="menu" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
+          <div  id="pencet"
+                className={`${isHamburgerOpen ? 'Diam': ''}`}  
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      }
     </div>
   )
 }
