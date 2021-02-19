d24edd3f16be323306a26318528efa3c29274eb9
adebayo <kurdzsamuel@gmail.com>
2021-02-19 05:07:32 +0100

    header slider overlay, learned about useRef

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useMediaQuery } from '../../utils/useMediaQuery';
 import CustomButton from '../custom-button/custom-button';
 import './header.scss';
@@ -8,8 +7,14 @@ import './header.scss';
 
 const Header = () => {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  let overLay = useRef();
 
   let isPageWide = useMediaQuery('(min-width: 800px)');
+  const openNavigation = () => {
+    setIsHamburgerOpen(!isHamburgerOpen);
+    document.body.classList.toggle('navOpenBodyUnscrollable');
+    overLay.current.classList.toggle('overlay-opened');
+  }
 
   return (
     <div className="header">
@@ -24,16 +29,22 @@ const Header = () => {
             <CustomButton buttonText='Order Now' buttonType='accent'/>
           </div>
         ) :
-        <div id="menu" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
-          <div  id="pencet"
-                className={`${isHamburgerOpen ? 'Diam': ''}`}  
-          >
+        <div id="menu" onClick={openNavigation}>
+          <div id="pencet" className={`${isHamburgerOpen ? 'Diam': ''}`}>
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
       }
+      <div id="myNav" ref={overLay} className="overlay">
+        <div className="overlay-content">
+          <span className="overlay-link">Menu</span>
+          <span className="overlay-link">Our Story</span>
+          <span className="overlay-link">Locations</span>
+          <span className="overlay-link">Cart</span>
+        </div>
+      </div>
     </div>
   )
 }
