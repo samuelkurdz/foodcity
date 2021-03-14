f1b8986545335b7cde062294e0a22c9880f8a401
adebayo <kurdzsamuel@gmail.com>
2021-03-14 17:18:57 +0100

    adding id to foods

--- src/utils/fetchFoodMenuData.jsx
+++ src/utils/fetchFoodMenuData.jsx
@@ -11,6 +11,7 @@ import CupCakes from '../assets/images/cupcakes.jpg';
 export function useFoodMenu() {
   const foodMenu = [
     {
+      id: '1',
       name: 'moimoi',
       price: 300,
       imgUrl: Moimoi,
@@ -18,20 +19,23 @@ export function useFoodMenu() {
       category: ['lunch', 'dinner']
     },
     {
+      id: '2',
       name: 'pan cakes',
       price: 1000,
       imgUrl: PanCakes,
       rating: 4.0,
-      category: ['breakfast', 'dessert', 'lunch']
+      category: ['breakfast']
     },
     {
+      id: '3',
       name: 'Pap',
-      price: 800,
+      price: 400,
       imgUrl: Pap,
       rating: 2,
       category: ['breakfast', 'dessert']
     },
     {
+      id: '4',
       name: 'Pizza',
       price: 1100,
       imgUrl: Pizza,
@@ -39,6 +43,7 @@ export function useFoodMenu() {
       category: ['lunch', 'dinner']
     },
     {
+      id: '5',
       name: 'Cupcakes',
       price: 1500,
       imgUrl: CupCakes,
@@ -46,6 +51,7 @@ export function useFoodMenu() {
       category: ['lunch', 'dessert']
     },
     {
+      id: '6',
       name: 'Jollof rice and chicken',
       price: 2000,
       imgUrl: JollofChicken,
@@ -53,6 +59,7 @@ export function useFoodMenu() {
       category: ['breakfast', 'dinner', 'lunch']
     },
     {
+      id: '7',
       name: 'Beef jerky',
       price: 1600,
       imgUrl: Beef,
@@ -60,6 +67,7 @@ export function useFoodMenu() {
       category: ['lunch', 'breakfast']
     },
     {
+      id: '8',
       name: 'Meat Servings',
       price: 2100,
       imgUrl: Servings,
@@ -67,6 +75,7 @@ export function useFoodMenu() {
       category: ['lunch', 'dinner']
     },
     {
+      id: '9',
       name: 'Fruit salad',
       price: 2500,
       imgUrl: FriutSalad,
